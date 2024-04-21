3405c06a9b9d29bc33fedaf807ade762afdd2021
AndreyVedineev <avedineev69@gmail.com>
2024-04-21 09:46:42 +0500

    21.04

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 
 
-from django.http import HttpResponse 
+from django.http import HttpResponse
+
+from catalog.models import Author, Book, BookInstance
+
 
 def index(request):
-    text_head = 'Это заголовок главной страницы сайта'
-    text_body = 'Это содержимое главной страницы сайта'
-    context = { 'text_head': text_head, 'text_body': text_body}
-    return render(request, 'catalog/index.html', context)
+    text_head = "На нашем сайте вы можете nолучить книги в электронном виде"
+    books = Book.objects.all()
+    num_books = Book.objects.all().count
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status_exact=2).count()
+    authors = Author.objects
+    num_authors = Author.objects.count
+    context = {
+        'text_head': text_head,
+        "books": books,
+        "num_books": num_books,
+        "num_instances": num_instances,
+        "num_instances_available": num_instances_available,
+        "authors": authors,
+        "num_authors": num_authors,
+        'text_body': "Это содержимое главной страницы сайта",
+    }
+
+    return render(request, "catalog/index.html", context)
