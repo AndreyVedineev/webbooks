5601c94dd5af4225fc5cec0c1294cd8148c7a141
Andrew Vedineev <avedineev@yandex.ru>
2024-04-21 10:04:22 +0300

    base and index already

--- catalog/views.py
+++ catalog/views.py
@@ -1,28 +1,26 @@
 from django.shortcuts import render
 
 
-from django.http import HttpResponse
-
 from catalog.models import Author, Book, BookInstance
 
 
 def index(request):
-    text_head = "На нашем сайте вы можете nолучить книги в электронном виде"
+    text_head = "На нашем сайте вы можете получить книги в электронном виде"
     books = Book.objects.all()
     num_books = Book.objects.all().count
     num_instances = BookInstance.objects.all().count()
-    num_instances_available = BookInstance.objects.filter(status_exact=2).count()
+    num_instances_available = BookInstance.objects.filter(status__exact=2).count()
     authors = Author.objects
     num_authors = Author.objects.count
     context = {
-        'text_head': text_head,
+        "text_head": text_head,
         "books": books,
         "num_books": num_books,
         "num_instances": num_instances,
         "num_instances_available": num_instances_available,
         "authors": authors,
         "num_authors": num_authors,
-        'text_body': "Это содержимое главной страницы сайта",
+        "text_body": "Это содержимое главной страницы сайта",
     }
 
     return render(request, "catalog/index.html", context)
